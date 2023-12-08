5767622309237e5a9815ef18fe0c4afe2d56eef2
Gold1377 <150453841+Gold1377@users.noreply.github.com>
2023-12-08 07:18:27 -0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,7 @@
 #importing modules and widgets
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout,QHBoxLayout, QPushButton, QLabel, QLineEdit
- 
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout,QHBoxLayout, QPushButton, QLabel, QLineEdit,QListWidget,QMessageBox
+import json
 #declaring constants
 win_width, win_height = 200, 300
 win_x, win_y = 200, 200
@@ -16,44 +16,131 @@ class MainWindow(QWidget):
         self.initUI()
         #connects the elements
         self.connects()
- 
+        self.dict=dict()
+        self.load_data()
         #determines how the window will look (text, size, location)
         self.set_appear()
  
         # start:
         self.show()
- 
+    def load_data(self):
+        try:
+            with open("save.json", "r")as inputfile: 
+                self.dict=json.load(inputfile)
+                self.addlist()
+        except:
+            print('no data to read')
+    
+                
     def initUI(self):
         ''' creates graphical elements '''
-        self.btn_add = QPushButton("+", self)
-        self.lable_finish = QLabel()
-        self.btn_scad = QPushButton("-", self)
-        self.btn_1 = QPushButton("1", self)
-        self.btn_2 = QPushButton("2", self)
-        self.btn_3 = QPushButton("3", self)
-        self.btn_4 = QPushButton("4", self)
+        intrebare=QLabel('intrebare',self)
+        corect=QLabel('raspuns corect',self)
+        r1=QLabel('raspuns gresit 1',self)
+        r2=QLabel('raspuns gresit 2',self)
+        r3=QLabel('raspuns gresit 3',self)
+        self.corectedit=QLineEdit()
+        self.intrebareedit=QLineEdit()
+        self.r1edit=QLineEdit()
+        self.r2edit=QLineEdit()
+        self.r3edit=QLineEdit()
+        self.edit=QListWidget()
+        self.edit.setMinimumWidth(120) 
+        self.edit.setMinimumHeight(120)
+        self.intrnou=QPushButton('intrebare noua',self)
+        self.sterintr = QPushButton("sterge intrebarea", self)
+        self.exers=QPushButton('incepe sa exersezi',self)
         self.layout_line = QVBoxLayout()
  
-        self.layout_line.addWidget(self.btn_add, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.lable_finish, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.btn_scad, alignment = Qt.AlignCenter)   
+        
  
- 
-        layoutH = QHBoxLayout()
-        layoutH.addWidget(self.btn_1, alignment = Qt.AlignCenter)
-        layoutH.addWidget(self.btn_2, alignment = Qt.AlignCenter)
-        layoutH.addWidget(self.btn_3, alignment = Qt.AlignCenter)
-        layoutH.addWidget(self.btn_4, alignment = Qt.AlignCenter)
- 
-        my_Widget = QWidget()
-        my_Widget.setLayout(layoutH)
-        self.layout_line.addWidget(my_Widget)
- 
-        self.lable_finish.setText("Calculator Mockup")       
+        L=QVBoxLayout()
+        l1 = QHBoxLayout()
+        l1.addWidget(self.edit)
+        
+        l2=QVBoxLayout()
+        l2.addWidget(intrebare)
+        l2.addWidget(corect)
+        l2.addWidget(r1)
+        l2.addWidget(r2)
+        l2.addWidget(r3)
+        l3=QVBoxLayout()
+        l3.addWidget(self.intrebareedit)
+        l3.addWidget(self.corectedit)
+        l3.addWidget(self.r1edit)
+        l3.addWidget(self.r2edit)
+        l3.addWidget(self.r3edit)
+        l1.addLayout(l2)
+        l1.addLayout(l3)
+        l5=QHBoxLayout()
+        l5.addWidget(self.intrnou)
+        l5.addWidget(self.sterintr)
+        l6=QHBoxLayout()
+        l6.addWidget(self.exers)
+        L.addLayout(l1)
+        L.addLayout(l5)
+        L.addLayout(l6)
+        
+        
+        
+        self.layout_line.addLayout(L)
         self.setLayout(self.layout_line)
- 
- 
+    def addNewQuestion(self):
+        question=self.intrebareedit.text()
+        ct=self.corectedit.text()
+        r1t=self.r1edit.text()
+        r2t=self.r2edit.text()
+        r3t=self.r3edit.text()
+        msg = QMessageBox()
+        msg.setWindowTitle("raspuns lipseste")
+        if r1t=='':
+            msg.setText("lipseste raspunsul gresit 1")
+            msg.exec_() 
+        elif r2t=='':
+            msg.setText("lipseste raspunsul gresit 2")
+            msg.exec_() 
+        elif r3t=='':
+            msg.setText("lipseste raspunsul gresit 3")
+            msg.exec_() 
+        elif ct=='':
+            msg.setText("lipseste raspunsul corect ")
+            msg.exec_() 
+        elif question=='':
+            msg.setText("lipseste intrebarea ")
+            msg.exec_() 
+        else:
+            self.dict[question]={'cq':ct,'r1':r1t ,'r2':r2t,'r3':r3t}
+            print(self.dict)
+            self.addlist()
+            with open("save.json", "w") as outfile: 
+                jsonsave=json.dumps(self.dict)
+                outfile.write(jsonsave)
+    def deleteb(self):
+        for key in self.dict.keys():
+            if self.edit.isItemSelected(key):
+                del self.dict[key]
+                self.addlist()
+        # listItems=list(self.edit.selectedItems())
+        # print(listItems)
+        # if listItems: 
+        #     for item in listItems:
+        #         print(item)
+        #         del self.dict[item] 
+
+        #         self.addlist() 
+        #         with open("save.json", "w") as outfile: 
+        #             jsonsave=json.dumps(self.dict)
+        #             outfile.write(jsonsave)
+             
+    def addlist(self):
+        self.edit.clear()
+        items=self.dict.keys()
+        self.edit.addItems(items)
+
+
     def connects(self):
+        self.intrnou.clicked.connect(self.addNewQuestion)
+        self.sterintr.clicked.connect(self.deleteb)
         # Nu need for the buttons to do something in this exercise/nothing to add here 
         pass
  
